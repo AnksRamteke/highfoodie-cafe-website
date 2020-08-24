910d889ec75f7484ed8ed70ebed863148b776a25
Ankit Ramteke <59204926+AnksRamteke@users.noreply.github.com>
2020-08-24 18:14:07 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("about", views.about, name = 'about'),
     path("FoodItem", views.FoodItem, name = 'FoodItem'),
     path("contact", views.contact, name = 'contact'),
+    path("search", views.search, name = 'search'),
 ]
