76bedb5784349192b37db965662371e72b25bdd9
Ankit Ramteke <59204926+AnksRamteke@users.noreply.github.com>
2020-08-24 12:34:59 +0530

    Update models.py

--- home/models.py
+++ home/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
+    sno = models.Autofield(primary_key = True)
     name =models.CharField(max_length = 120)
     email =models.CharField(max_length = 120)
     phone = models.CharField(max_length = 12)
-    date = models.DateField()
+    content = models.Textfield()
+    timestamp = models.DateTimeField(auto_now_add=True, blank = True )
 
 
     def __str__(self):
